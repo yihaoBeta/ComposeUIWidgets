fc63712f64c033634e995b3c04e248ced73ca672
yihao <yihaobeta@163.com>
2023-04-26 14:35:11 +0800

    initialize commit

---
+++ library/src/main/java/com/yihao/library/extensions/rotation.kt
@@ -0,0 +1,20 @@
+package com.yihao.library.extensions
+
+import androidx.compose.animation.core.InfiniteRepeatableSpec
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+import androidx.compose.ui.draw.rotate
+
+fun Modifier.infiniteRotate(): Modifier = composed {
+    val infiniteTransition = rememberInfiniteTransition()
+    val rotate by infiniteTransition.animateFloat(
+        initialValue = 0F,
+        targetValue = 360F,
+        animationSpec = InfiniteRepeatableSpec(animation = tween(durationMillis = 1000))
+    )
+    this.then(Modifier.rotate(rotate))
+}
